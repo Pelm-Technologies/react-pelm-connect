fa1d9e48506f85b8700f56661f021bd596739142
Edmund Tian <edmundtian@ezt.local>
2022-02-24 16:48:21 -0800

    https

--- src/useConnect.tsx
+++ src/useConnect.tsx
@@ -24,7 +24,7 @@ export const useConnect = (config: Config) => {
 
     const [error, setError] = useState<Error | undefined | null>()
 
-    const [loading, scriptError] = useScript({ src: 'http://api.pelm.com/connect/pelm-connect.js', checkForExisting: true });
+    const [loading, scriptError] = useScript({ src: 'https://api.pelm.com/connect/pelm-connect.js', checkForExisting: true });
     // const [loading, scriptError] = useScript({ src: 'https://pelm-staging.herokuapp.com/connect/pelm-connect.js', checkForExisting: true });
     // const [loading, scriptError] = useScript({ src: 'http://127.0.0.1:5000/connect/pelm-connect.js', checkForExisting: true });
     
